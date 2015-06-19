53cbce09d25358f72a21102b1b3747d408aabe71
Alec Bibat <alecbibat@gmail.com>
2015-06-19 16:45:28 -0600

    set up initial game

--- main.rb
+++ main.rb
@@ -28,6 +28,20 @@ post "/new_player" do
 end
 
 get "/game" do
+  suits = ['C', 'H', 'D', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:player_cards] = []
+  session[:dealer_cards] = []
+
+  2.times do
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  end
+
   # set up initial values
   # render the template
+  # add redirect back to '/' if no player name exists
+  erb :game
 end
