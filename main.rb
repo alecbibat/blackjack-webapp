1ce23aa5202bc98c6e95222bd1dbdf1ddb39ea33
Alec Bibat <alecbibat@gmail.com>
2015-06-20 14:39:34 -0600

    hide hit-stay if bust or stay

--- main.rb
+++ main.rb
@@ -7,6 +7,28 @@ require 'sinatra/base'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(cards) #cards is a nested array
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else 
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+
+    #correct for aces
+    arr.select{|element| element == "A"}.count.times do
+      break if total <= 21
+      total -= 10
+    end
+
+    total
+  end
+end
 
 get '/' do
   # This should validate that a username is set
@@ -27,6 +49,10 @@ post "/new_player" do
   redirect "/game"
 end
 
+before do
+  @show_buttons = true
+end
+
 get "/game" do
   suits = ['C', 'H', 'D', 'S']
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -45,3 +71,23 @@ get "/game" do
   # add redirect back to '/' if no player name exists
   erb :game
 end
+
+post '/game/player/stay' do
+  @success = "You chose to stay. Boring."
+  @show_buttons = false
+  erb :game
+end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > 21
+    @error = "Sorry! You busted!"
+    @show_buttons = false
+    erb :game
+  else
+    erb :game
+  end
+  # check if player busted
+  # if bust, game over
+end
+
