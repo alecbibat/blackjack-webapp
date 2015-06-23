031c1936b58748e1682f04611f20d621be64115e
Alec Bibat <alecbibat@gmail.com>
2015-06-22 19:21:06 -0600

    ajaxify player_stay and dealer_hit

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'pry'
-
+require 'sinatra/contrib'
+require 'sinatra/reloader'
+require 'sinatra/base'
 
 set :sessions, true
 
--- public/application.js
+++ public/application.js
@@ -11,3 +11,31 @@ $(document).ready(function(){
     });
   });
 });
+
+$(document).ready(function(){
+  $('form#stay_button input').click(function(){
+    $(document).on('click', 'form#stay_button input', function(){
+      $.ajax({
+        type: 'POST',
+        url: '/game/player/stay',
+      }).done(function(msg){
+        $('#game').replaceWith(msg)
+      });
+      return false;
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('form#dealer_hit_button input').click(function(){
+    $(document).on('click', 'form#dealer_hit_button input', function(){
+      $.ajax({
+        type: 'POST',
+        url: '/dealer/hit',
+      }).done(function(msg){
+        $('#game').replaceWith(msg)
+      });
+      return false;
+    });
+  });
+});
